d8d4e880cade70b779dd6cf7de9ca5cf1b95396e
SAMO <samolson@reed.edu>
2017-02-13 18:50:13 -0800

    Revisions made to Chapter 1 & 2 text. Organizational changes made to push bibliography to end & include an Appendix.

--- research-log/stickystat.R
+++ research-log/stickystat.R
@@ -13,11 +13,18 @@ for(i in seq_along(pages)) {
 }
 # revised code to check if a move is used
 stickyreturn <- function(battlelog) { 
-  if(any(grep("(Sticky Web[|]p1a)(Sticky Web[|]p2a)", battlelog)) == TRUE) {stickyuse <- "both"} 
+  if(any(grep("(Sticky Web[|]p1a) | (Sticky Web[|]p2a)", battlelog)) == FALSE) {
+    stickyuse <- NA
+  }
+  if(any(grep("(Sticky Web[|]p1a) && (Sticky Web[|]p2a)", battlelog)) == TRUE) {
+    stickyuse <- "both"
+    } 
   if(any(grep("[|]Sticky Web[|]p1a", battlelog)) == TRUE) {
-    stickyuse <- "p1"} 
-  if(any(grep("(Sticky Web[|]p2a)", battlelog)) == TRUE) {stickyuse <- "p2"}
-  else {stickyuse <- NA}
+    stickyuse <- "p1"
+    } 
+  if(any(grep("[|]Sticky Web[|]p2a", battlelog)) == TRUE) {
+    stickyuse <- "p2"
+  } 
   stickyuse
 }
 
@@ -29,7 +36,7 @@ for(i in 1:length(pages)) { stickyverdict[i] <- stickyreturn(pages[[i]]$log)}
 # then the code checks if p1 won
 # if p1 did not win and it was not a draw, then p2 won 
 playerout <- function(battlelog) {
-  if((battlelog$endType) == "draw"){
+  if((battlelog$endType) == "draw") {
     playout <- NA} 
   if((battlelog$p1rating$elo > battlelog$p1rating$oldelo) == TRUE) {
     playout <- "p1"}
@@ -46,19 +53,32 @@ for(i in 1:length(pages)) {winlose[i] <- playerout(pages[[i]])}
 # 1. the move was not used or 2. both players used the move 
 # then the code checks if the winning player used the move and returns a 1 if they did
 # otherwise the function outputs a 0 if the player who used the move lost and their opponent did not use the move
-stickystat <- function(stickyverdict, winlose) {
-  if((winlose=stickyverdict)==TRUE) {
-    stickywin <- 1}
-  if(winlose="p1" & stickyverdict="p2") {
-    stickywin <-0}
-  if(winlose="p2" & stickyverdict="p2") {
-    stickywin <-0}
-  else {
-    stickywin <- NA}
+stickystat <- function(playerout, stickyreturn, battlelog) {
+  if((winlose=="p1") && (stickyverdict=="p1")) {
+    stickywin <-1
+  }
+  if((winlose=="p2") && (stickyverdict)=="p2") {
+    stickywin <-1
+  }
+  if((winlose=="p1") && (stickyverdict=="p2")) {
+    stickywin <-0
+  }
+  if((winlose=="p2") && (stickyverdict=="p1")) {
+    stickywin <-0
+  }
+  if(is.na(stickyverdict)) {
+    stickywin <- NA
+  }
+  if(is.na(winlose)) {
+    stickywin <- NA
+  }
   stickywin 
 }
-
-stickystat(stickyverdict[i], winlose[i])  
+# unused atm
+## which stickystat to use? 
+stickystat(stickyverdict[[i]], winlose[[i]])  
+stickystat(pages[[i]]) 
+## stickyout, function of stickystat
 stickyout <- rep(NA, length(pages))
-for(i in 1:length(pages)) {stickyout[i] <- stickystat(stickyverdict[i], winlose[i])} 
+for(i in 1:length(pages)) {stickyout[i] <- stickystat(playerout(pages[i]), winlose(pages[i]))} 
 # whenever I use this code it just returns an "unexpected '}' in "}" error 
